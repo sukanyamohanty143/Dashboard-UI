4f3760653ac50449037bb322f69cd348e3331bfa
Sukanya Mohanty <90206460+sukanyamohanty143@users.noreply.github.com>
2022-12-28 21:04:36 -0800

    first commit

---
+++ dashboar-ui/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Mainbody from './Mainbody';
+
+function App() {
+  return (
+    <div className='main-content1'>
+      <Mainbody/>
+    </div>
+  );
+}
+
+export default App;
---
+++ dashboar-ui/src/Mainbody.js
@@ -0,0 +1,31 @@
+
+export default function Mainbody() {
+    let item="<"
+    return (
+        <div className='main-content2'>
+            <div className='content1'>
+                <div className="side1">Dashboard</div>
+                <div className="side2">Employee{item}</div>
+                <div className="side3">Attendance{item}</div>
+                <div className="side4">slte/Field Track{item}</div>
+                <div className="side5">Payroll{item}</div>
+                <div className="side6">Statuatory Compliances{item}</div>
+                <div className="side7">Leaves{item}</div>
+                <div className="side8">Asset Management{item}</div>
+                <div className="side9">Golas & Performanc{item}</div>
+                <div className="side10">Recruitment{item}</div>
+                <div className="side11" >Openings</div>
+                <div className="side12">Candidates</div>
+                <div className="side13">Interview Process</div>
+            </div>
+            <div className='content2'>
+                <div className="navBar">
+                    <h4 className="title">INTERVIEW PROCESS</h4>
+                    <div className="close">X</div>
+                    <h5>Name<span>*</span></h5>
+                </div>
+                
+            </div> 
+        </div>
+    );
+};
